6dc77cdba46ec4f712941dfdea8ade57d285df16
Nikhil Thadani <nikhilthadani112256@gmail.com>
2022-01-17 19:10:53 +0530

    Final

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Loader, Sprite, Texture } from "pixi.js";
+import { AnimatedSprite, Application, Loader, Texture } from "pixi.js";
 
 const app = new Application({
   width: innerWidth,
@@ -9,31 +9,51 @@ document.body.appendChild(app.view);
 
 const loader = Loader.shared;
 
-// Load assets
 loader
-  .add("img1", "./assets/1.png")
-  .add("img2", "./assets/2.png")
-  .load((l) => {
-    // Convert to texture
-    const texture = Texture.from("img1");
-    const texture2 = Texture.from("img2");
-    console.log(texture, texture2);
+  .add("./assets/spritesheet.json")
+  .add("./assets/fighter.json")
+  .load(() => {
+    const textures: Texture[] = [];
 
-    // Convert to Sprites
+    for (let i = 1; i <= 4; i++) {
+      const texture = Texture.from(`RunRight0${i}.png`);
+      textures.push(texture);
+    }
+    console.log(textures);
 
-    const sprite = new Sprite(texture);
-    console.log(sprite);
+    const animatedSprite = new AnimatedSprite(textures);
+    animatedSprite.x = 200;
+    animatedSprite.y = 100;
+    animatedSprite.play(); // Will run the animation
 
-    sprite.position.set(100, 100);
-    sprite.width = 100;
-    sprite.height = 100;
+    app.stage.addChild(animatedSprite);
+    animatedSprite.animationSpeed = 0.5;
 
-    app.stage.addChild(sprite);
+    const jetArray = [];
+    for (let i = 0; i <= 29; i++) {
+      const texture = Texture.from(`rollSequence00${i}.png`);
+      jetArray.push(texture);
+    }
+    console.log(jetArray);
 
-    const sprite2 = new Sprite(texture2);
-    sprite2.position.set(200, 200);
-    sprite2.width = 100;
-    sprite2.height = 100;
+    const jetSprite = new AnimatedSprite(jetArray);
+    jetSprite.x = 200;
+    jetSprite.y = 200;
+    jetSprite.play();
+    app.stage.addChild(jetSprite);
 
-    app.stage.addChild(sprite2);
+    function backToPosition() {
+      if (jetSprite.y == -100) {
+        jetSprite.y = screen.height;
+      }
+
+      if (animatedSprite.x === 1000) {
+        animatedSprite.x = 10;
+      }
+    }
+    app.ticker.add(() => {
+      animatedSprite.x += 10;
+      jetSprite.y -= 10;
+      backToPosition();
+    });
   });
