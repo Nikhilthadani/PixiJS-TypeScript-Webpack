304f4bfad760b068de260ac46458d28f9e5875ea
Nikhil Thadani <nikhilthadani112256@gmail.com>
2022-01-20 15:57:05 +0530

    Final

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Loader, Application, Sprite } from "pixi.js";
+import { Loader, Application, Sprite, Graphics } from "pixi.js";
 
 const app = new Application({
   width: innerWidth,
@@ -8,14 +8,14 @@ const app = new Application({
 document.body.appendChild(app.view);
 
 const loader = Loader.shared;
-
 loader.add("img", "./assets/1.png").load((l) => {
+  const graphics = new Graphics();
+  graphics.beginFill();
+  graphics.drawRect(100, 100, 150, 150);
+  graphics.endFill();
+  app.stage.addChild(graphics);
   const sprite = Sprite.from("img");
   app.stage.addChild(sprite);
-  sprite.position.set(500);
-  sprite.anchor.set(0.5);
 
-  app.ticker.add(() => {
-    sprite.rotation += 0.01;
-  });
+  sprite.mask = graphics;
 });
